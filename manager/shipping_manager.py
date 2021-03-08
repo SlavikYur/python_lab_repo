31232f6be199890a749822f77caf0f2c1aa1e96e
SlavikYur <mr.sluvik822@gmail.com>
2021-03-08 17:33:59 +0200

    Adding files of Lab3

---
+++ manager/shipping_manager.py
@@ -0,0 +1,60 @@
+from enum import Enum, auto
+from models.item_ordered import ItemType
+from models.shipping_type import Courier, Postamat, Department
+from models.company import Company
+from models.shipping import Shipping
+
+class SortParameter(Enum):
+
+    DELIVERY_TIME = Shipping.get_delivery_time
+    PRICE = Shipping.calc_shipping_price
+
+
+class SortOrder(Enum):
+
+    DESCENDING = True
+    ASCENDING = False
+
+
+class ShippingManager:
+
+    def __init__(self, items_ordered):
+        self.__items_ordered = items_ordered
+        self.__shipping_types = []
+        self.__shipping_options = []
+
+
+    def add_shipping_type(self, shipping_type):
+
+        self.__shipping_types.append(shipping_type)    
+
+
+    def create_shipping_options(self):
+
+        for shipping_type in self.__shipping_types:
+            self.__shipping_options.append(Shipping(self.__items_ordered, shipping_type))
+
+
+    def display_shipping_options(self):
+
+        for shipping_option in self.__shipping_options:
+            print(shipping_option)   
+
+
+    def search_by_price(self, lower_bound=0, upper_bound='inf'):
+
+        options = []
+
+        for shipping in self.__shipping_options:
+
+            if  lower_bound <= shipping.calc_shipping_price() <= float(upper_bound):
+                options.append(shipping)
+
+        options.sort(key=SortParameter.DELIVERY_TIME)        
+
+        return options
+
+
+    def sort_option_by(self, sort_parameter, sort_order=SortOrder.ASCENDING):
+
+        self.__shipping_options.sort(key=sort_parameter, reverse=sort_order.value)
---
+++ models/shipping_type.py
@@ -0,0 +1,39 @@
+from models.company import Company
+
+class ShippingType:
+
+    def __init__(self, shipping_type_name, company : Company):
+
+        self._shipping_type_name = shipping_type_name
+        self._company = company
+
+
+class Courier(ShippingType):
+
+    def __init__(self, shipping_type_name, company : Company, name, age, phone_number):
+
+        ShippingType.__init__(self, shipping_type_name, company)
+        self._name = name
+        self._age = age
+        self._phone_number = phone_number
+
+
+
+class Postamat(ShippingType):
+
+    def __init__(self, shipping_type_name, company : Company, location, id):
+
+        ShippingType.__init__(self, shipping_type_name, company)
+        self._location = location
+        self._id = id
+
+
+
+class Department(ShippingType):
+
+    def __init__(self, shipping_type_name, company : Company, location, id):
+
+        ShippingType.__init__(self, shipping_type_name, company)
+        self._location = location
+        self._id = id
+
