6cf57af1139b157d93155660d01dc0b6ec70b0f5
SlavikYur <74894929+SlavikYur@users.noreply.github.com>
2021-03-29 11:50:26 +0300

    Optimized the search

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ def main():
     with open(os.path.join(os.path.dirname(__file__), 'access_log_Jul95'), 'r') as file:
         REGEX_SUCCESSFUL_MOVIE_REQUEST = re.compile(
             r'([a-zA-Z0-9._-]+)'
-            '( - - )'
-            '(\[01/Jul/1995:(00:\d{2}:\d{2}|01:00:00) -\d{4}\])'
-            ' \"([a-zA-Z0-9. _/-]+)'
-            'movies'
-            '([a-zA-Z0-9._/-]+)'
-            '.mpg'
-            ' ([a-zA-Z0-9._/-]+)\"'
-            ' 200 \d+')
+            r'( - - )'
+            r'(\[01/Jul/1995:(00:\d{2}:\d{2}|01:00:00) -\d{4}\])'
+            r' \"([a-zA-Z0-9. _/-]+)'
+            r'movies'
+            r'([a-zA-Z0-9._/-]+)'
+            r'.mpg'
+            r' ([a-zA-Z0-9._/-]+)\"'
+            r' 200 \d+')
 
-        matches = REGEX_SUCCESSFUL_MOVIE_REQUEST.finditer(file.read())
+        REGEX_STOP_SEARCH = re.compile(r'(01/Jul/1995:(01:\d{2}:\d[1-9]))')
+        
+        matches = []
+        for line in file:
+            match = REGEX_SUCCESSFUL_MOVIE_REQUEST.search(line)
+            stop_match = REGEX_STOP_SEARCH.search(line)
+            if match:
+                matches.append(match)
+            if stop_match:
+                break
 
         for match in matches:
             print(match.group())
